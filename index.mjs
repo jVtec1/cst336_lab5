acf39209dc3bfdea1a5419d71367ac956f45eadc
Andy Espinoza <andyesp831@gmail.com>
2025-03-13 14:03:59 -0700

    w8 participation + extra credit

--- index.mjs
+++ index.mjs
@@ -4,21 +4,29 @@ import fetch from 'node-fetch';
 import path from "path";
 import { fileURLToPath } from "url";
 
+
 const planets = (await import('npm-solarsystem')).default;
+const date = new Date();
 //needed to use node with vercel
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+//api used for this project
 let url = "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=solar%20system&per_page=50&orientation=horizontal";
 let response = await fetch(url);
 let data = await response.json();
+//size is for homepage use
 let size = Object.keys(data.hits).length;
+
 
 const app = express();
 app.set("view engine", "ejs");
+
 //needed to use node with vercel
 app.set("views", path.join(__dirname, "views")); // Ensure this points to the correct directory
 app.use(express.static("public"));
+
 
 app.get('/', (req, res) => {  
     let rand = Math.floor(Math.random() * size);
@@ -56,14 +64,96 @@ app.get('/saturn', (req, res) => {
             saturn:saturnInfo
         });
 });
-app.get('/nasa', async (req, res) => {
-    let url2 = "https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=2025-03-06";
+app.get('/nasaToday', async (req, res) => {
+    
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let tday = year + "-" + month + "-" + day;
+
+    let url2 = `https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=${tday}`;
     let response2 = await fetch(url2);
     let data2 = await response2.json();
     res.render('nasa',
         {
             nasa:data2
         });
+});
+app.get('/nasaCustom', async (req, res) => {
+    let day = req.query.day;
+    let month = req.query.month;
+    let year = req.query.year;
+    let tday = year + "-" + month + "-" + day;
+    let url2 = `https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=${tday}`;
+    let response2 = await fetch(url2);
+    let data2 = await response2.json();
+    res.render('nasa',
+        {
+            nasa:data2
+        });
+});
+app.get('/getPlanetInfo', (req, res) => {  
+    let planetSelect = req.query.planet;
+    let planetInfo;
+    switch(planetSelect) {
+        case "Mercury":
+            planetInfo = planets.getMercury();
+            break;
+
+        case "Venus":
+            planetInfo = planets.getVenus();
+            break;
+
+        case "Earth":
+            planetInfo = planets.getEarth();
+            break;
+
+        case "Mars":
+            planetInfo = planets.getMars();
+            break;
+        
+        case "Jupiter":
+            planetInfo = planets.getJupiter();
+            break;
+
+        case "Saturn":
+            planetInfo = planets.getSaturn();
+            break;
+
+        case "Uranus":
+            planetInfo = planets.getUranus();
+            break;
+
+        case "Neptune":
+            planetInfo = planets.getNeptune();
+            break;
+
+        default:
+          // code block
+            break;
+      }
+    res.render('planetInfo',
+        {
+            planet:planetInfo,
+            name:planetSelect
+        });
+});
+app.get('/getComAstInfo', (req, res) => {  
+    let objSelect = req.query.Obj;
+    let objInfo;
+    if(objSelect == "Comets")
+    {
+        objInfo = planets.getComets();
+    }else{
+        objInfo = planets.getAsteroids();
+    }
+    
+    res.render('astComInfo',
+        {
+            obj:objInfo,
+            name:objSelect
+        }
+    )
 });
 
 const PORT = process.env.PORT || 3000;
